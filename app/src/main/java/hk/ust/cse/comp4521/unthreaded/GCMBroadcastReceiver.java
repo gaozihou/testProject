b34dbb84a1d552dc2b15d594433275a7690fedab
Paul Gao <gaozihou@sina.com>
2015-04-06 16:36:55 +0800

    add GCM halfway

---
+++ app/src/main/java/hk/ust/cse/comp4521/unthreaded/GCMBroadcastReceiver.java
@@ -0,0 +1,13 @@
+package hk.ust.cse.comp4521.unthreaded;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.WakefulBroadcastReceiver;
+
+public class GCMBroadcastReceiver extends WakefulBroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
---
+++ app/src/main/java/hk/ust/cse/comp4521/unthreaded/GCMIntentService.java
@@ -0,0 +1,16 @@
+package hk.ust.cse.comp4521.unthreaded;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class GCMIntentService extends IntentService {
+
+    public GCMIntentService(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+}
